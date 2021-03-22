b8f536cde7d75188e576b372c37eeb31bc8d08d3
nunso478 <nunso478@gmail.com>
2021-03-22 11:35:16 +0000

    novos

--- ficha2/index.js
+++ ficha2/index.js
@@ -4,7 +4,7 @@
 //outputs
 //imc
 // assinatura da funçao
-function calcularIMC(weigh,height){
+/* function calcularIMC(weigh,height){
     var imc = weigh / Math.pow(height,2);
     return imc;
 }
@@ -99,4 +99,197 @@ function countletter(str,letter) {
     return count;
 }
 var count = countletter("hellE","e");
-console.log(count);
+console.log(count);  */
+//EXERCICIO 5 ==============================================
+
+/*function calcularWorking(he,me,se,hs,ms,ss){
+    if (he < 8 || hs > 18) {
+        console.log("horario nao permitido");
+        return;
+    }
+var enteraInseconds = he * 3600 + me * 60 + se;
+var exitInSeconds = hs * 3600 + ms * 60 + ss;
+
+var timeInSeconds = exitInSeconds - enteraInseconds;
+
+var remainderhours= timeInSeconds % 3600;
+
+var hours = (timeInSeconds - remainderhours) / 3600;
+var remainderminutes = remainderhours % 60;
+
+var minutes = (remainderhours - remainderminutes) / 60;
+
+console.log("tempo de trabalho: " + hours + ":"+ minutes +":"+ remainderminutes);
+
+
+
+var seconds = 0;
+ 
+}
+calcularWorking(7,30,0,16,0,0);
+//exercicio 6 ==========================================
+/*function drawRectanngle(width,height){
+  
+   
+    var line = "";
+   
+    for (let index = 0; index < width; index++) {
+        line += "*";
+         
+       }
+    for (let j = 0; j < height; j++) {
+       console.log(line); 
+       
+    }
+   
+}
+drawRectanngle(5,3);*/
+// exercicio 7 ====================
+
+/*function drawRectriangle(height){
+    var line = "";
+    for (let j = 0; j <= height; j++) {
+        line += "*";
+        console.log(line);
+        
+    }
+    
+   
+}
+drawRectriangle(5);*/
+
+// exercicio 8 =======================´
+
+/*function drawRectanngle(width,height){
+ 
+    for (let index = 0; index < width; index++) {
+        var line = "";
+        for (let j = 0; j < height; j++) {
+            if(index == 0 || index == height -1 || j == 0 || j == width -1){
+                line +="*"
+            }
+            else{
+                line += " "
+            }
+       
+        }
+        console.log(line)
+   }
+}
+drawRectanngle(10,10);*/
+
+// exercicio 9 ========================
+
+var student1 = {firstName:"Pedro",LastName:"Perreira",age:20,grade:16.5, getGrade:function(){return this.firstName + " " + this.LastName + " com idade: " + this.age + " com nota:" + this.grade}};
+var student2 = {firstName:"joao",LastName:"ferreira",age:21,grade:19,getGrade:function(){return this.firstName + " " + this.LastName + " com idade: " + this.age + " com nota:" + this.grade}};
+var student3 = {firstName:"miguel",LastName:"Nunes",age:22,grade:12,getGrade:function(){return this.firstName + " " + this.LastName + " com idade: " + this.age + " com nota:" + this.grade}};
+var student4 = {firstName:"andre",LastName:"Jorge",age:23,grade:15,getGrade:function(){return this.firstName + " " + this.LastName + " com idade: " + this.age + " com nota:" + this.grade}};
+var student5 = {firstName:"camacho",LastName:"Rena",age:24,grade:8.5,getGrade:function(){return this.firstName + " " + this.LastName + " com idade: " + this.age + " com nota:" + this.grade}};
+var student6 = {firstName:"joana",LastName:"Horta",age:20,grade:11,getGrade:function(){return this.firstName + " " + this.LastName + " com idade: " + this.age + " com nota:" + this.grade}};
+var student7 = {firstName:"Micaela",LastName:"Perreira",age:24,grade:18,getGrade:function(){return this.firstName + " " + this.LastName + " com idade: " + this.age + " com nota:" + this.grade}};
+var student8 = {firstName:"David",LastName:"Ferreira",age:19,grade:20,getGrade:function(){return this.firstName + " " + this.LastName + " com idade:" + this.age + " com nota:" + this.grade}};
+var student9 = {firstName:"Ines",LastName:"Perreira",age:25,grade:10,getGrade:function(){return this.firstName + " " + this.LastName + " com idade: " + this.age + " com nota: " + this.grade}};
+
+
+var studentarray = [];
+
+studentarray.push(student1);
+studentarray.push(student2);
+studentarray.push(student3);
+studentarray.push(student4);
+studentarray.push(student5);
+studentarray.push(student6);
+studentarray.push(student7);
+studentarray.push(student8);
+studentarray.push(student9);
+
+function displaygrade(array){
+    for (let i = 0; i < array.length; i++) {
+        const student = array[i];
+        console.log(student.getGrade());
+        
+    }
+
+}
+
+displaygrade(studentarray);
+//melhor nota 
+function GetbestGrade(array){
+    var max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if(array[i].grade > max.grade){
+            max = array[i];
+        }
+       
+        
+    }
+    return max;
+}
+var bestGrade = GetbestGrade(studentarray);
+console.log('melhor nota');
+console.log(bestGrade.getGrade());
+
+//pior nota 
+function piornota(array){
+    var min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if(array[i].grade < min.grade){
+            min = array[i];
+        }
+       
+        
+    }
+    return min;
+}
+var minnota = piornota(studentarray);
+console.log(' ');
+console.log('pior nota');
+console.log(minnota.getGrade());
+
+
+
+
+
+// verificaçao da nota negativa  
+function notanegativa (array){
+    console.log(" ");
+    console.log("nota negativas");
+    console.log(" ");
+    for (let i = 0; i < array.length; i++) {
+        const student = array[i];
+        if(array[i].grade < 9.5){
+          
+            console.log(student.getGrade());
+        }
+       
+        
+    }
+     
+
+}
+var negGrade = notanegativa(studentarray);
+/*console.log(' ');
+console.log('nota negativa');
+console.log(negGrade.getGrade());*/
+
+// verificaçao da nota positiva  
+function notapositiva (array){
+    console.log(" ");
+    console.log("nota positiva");
+    console.log(" ");
+    for (let i = 0; i < array.length; i++) {
+        const student = array[i];
+       
+        if(array[i].grade > 9.5){
+           
+            console.log(student.getGrade());
+        }
+    
+        
+    }
+
+}
+var posGrade = notapositiva(studentarray);
+/*console.log(' ');
+console.log('nota positiva');
+console.log(posGrade.getGrade());*/
