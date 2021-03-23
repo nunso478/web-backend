a0b4516f9c4a761868f72369b6bb92b380592140
nunso478 <nunso478@gmail.com>
2021-03-23 18:52:49 +0000

    novo

---
+++ ficha3/app.js
@@ -0,0 +1,25 @@
+function started( ) {
+    console.log("Started Download");
+    
+}
+function update(value) {
+        console.log(value +"% of Download completed")
+ 
+}
+function completed() {
+    console.log("completed Download")
+}
+function performDownload(startedF,updateF,completedF) {
+    startedF();
+    for (let i = 0; i <= 100; i++) {   
+          updateF(i);
+    }
+  
+    completedF();
+    
+}
+//performDownload(started,update,completed);
+
+//============================================================================
+var log = require("./log.js");
+console.log(log);
---
+++ ficha3/log.js
@@ -0,0 +1,11 @@
+var log = "hello Module!";
+
+var test = "hello test";
+
+
+
+
+
+
+
+module.exports = test;
