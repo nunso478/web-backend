b3394bb943258db9a0d6870cb6d6541cf42c5c65
nunso478 <nunso478@gmail.com>
2021-03-25 18:21:14 +0000

    novo

--- ficha3/ArrayUtils.js
+++ ficha3/ArrayUtils.js
@@ -52,7 +52,60 @@ var arrayUtils = {
         }
         return avg / array.length;
     },
+    indexOf: function(array,value){
+        for (let i = 0; i < array.length; i++) {
+           if(value == array[i]){
+               return i;
+           }
+            
+        }
+        return  -1;
+    },
+    subarray: function(array,starindeex,endindex){
+        var a = [];
+        for (let i = starindeex; i <= endindex; i++) {
+          a.push(array[i]);
+        }
+        return  a;
+    },
+    isSameLength: function(array,otherArray){
+    return array.length == otherArray.length;
+    },
+    reverse: function(array){
+        var r = [];
+        for (let i = array.length -1; i >= array.length; i--) {
+            r.push(array[i]);
 
+        }
+        return r;
+    },
+    swap: function(array,index1,index2){
+        //obter um valor que está num determinado indice do array
+         var val1 = array[index1];
+         var val2 = array[index2];
+         //alterar um determinado indice com outro valor 
+         array[index1] = val2;
+         array[index2] = val1;
+        return array;
+    },
+    contains: function(array,value){
+        /*for (let i = 0; i < array.length; i++) {
+           if(value == array[i]){
+               return true;
+           }
+            
+        }
+        return  false;*/
+        return this.indexOf(array,value) != -1;
+    },
+    concatenate: function(array,otherArray){
+        var concatenat = array;
+        for (let i = 0; i < otherArray.length; i++) {
+             concatenat.push(otherArray[i]);
+            
+        }
+        return concatenat;
+    },
 };
 
 module.exports = arrayUtils;
--- ficha3/app.js
+++ ficha3/app.js
@@ -24,7 +24,28 @@ const arrayUtils = require("./ArrayUtils.js");
 //============================================================================
 var log = require("./ArrayUtils.js");
 var array = [12,43,6,8,0];
+var otherArray = [7,10];
 console.log("o array está vazio? " + arrayUtils.isEmpty(array));
 console.log("o maximo do array é: " + arrayUtils.max(array));
 console.log("o minimo do array é: " + arrayUtils.min(array));
 console.log("a media do array é: " + arrayUtils.avarege(array));
+console.log("O indice do valor: " + 6 + " e: " + arrayUtils.indexOf(array,6));
+
+
+var subarray = arrayUtils.subarray(array,3,5);
+console.log("O sub-array é: " + subarray);
+
+var sameSize = arrayUtils.isSameLength(array,otherArray);
+console.log("Os arrays são do mesmo tamanho: "+ sameSize);
+
+var inverteArray = arrayUtils.reverse(array);
+console.log("O array invertido é: " + inverteArray);
+
+var swapArray = arrayUtils.swap(array, 0, 9);
+console.log("o array alterado: " + swapArray);
+
+console.log("O array contém o valor: " + arrayUtils.contains(array,6));
+
+
+var concatenateArray = arrayUtils.concatenate(array,otherArray);
+console.log("o array alterado: " + concatenateArray);
