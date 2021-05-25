19fde6e6aaee82af8483de30f5d583f50b04cebb
nunso478 <nunso478@gmail.com>
2021-05-25 19:51:19 +0100

    nova ficha10

---
+++ ficha10/controllers/personController.js
@@ -0,0 +1,14 @@
+
+const Person = require('../sequelize').Person;
+
+
+exports.getPersons = function (req, res, next) {
+    
+    Person.findAll()
+        .then(result => {
+            res.render('person',{title: 'Person', data: result})
+        });
+
+    
+
+}
