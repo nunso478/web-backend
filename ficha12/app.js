062bd2a1aab27b97536cdd41717c56878ca41a9c
nunso478 <nunso478@gmail.com>
2021-06-15 09:55:44 +0100

    novo

--- ficha12/app.js
+++ ficha12/app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
