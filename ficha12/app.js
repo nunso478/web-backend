52e20ca74281da28c1ccea40ee2fb8a856622d7a
nunso478 <nunso478@gmail.com>
2021-06-17 18:52:16 +0100

    novo

--- ficha12/app.js
+++ ficha12/app.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var zlib = require('zlib');
+
 
 function readfile(path) {
     var file = fs.readFile(path, (err,data)=> {
@@ -18,13 +20,24 @@ function readFileStream(path)
         writerStream.write(chunk,'utf-8');
     });
     readerStream.on('end', function(){      
+        console.log("file read");
         writerStream.end();
     });
     readerStream.on('error', function(err){
-        console.log(err.stack);
-    })
+        console.log(err,stack);
+    });
 }
 
 //readFile("text.txt");
-readFileStream("text.txt");
+//readFileStream("text.txt");
+
+var readerStream = fs.createReadStream('text.txt');
+var writerStream = fs.createWriteStream('copy.txt.gz');
+readerStream.pipe(writerStream);
+
+
+
+readerStream.pipe(zlib.createGzip()).pipe(writerStream);
+
+
 var x = 0;
